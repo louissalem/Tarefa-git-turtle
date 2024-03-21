2e757f837debbea2a1080ff1c4f6d423c5f69ce7
louissalem <162194600+louissalem@users.noreply.github.com>
2024-03-21 13:42:08 -0300

    Casa.py

--- casa.py
+++ casa.py
@@ -1,13 +1,15 @@
 import turtle
+import random
 def main():
     r = turtle.Screen()
     r.setup(width=500, height=500)
-    r.bgcolor("#CFF6FF")
+    r.bgcolor("#6D6EC4")
     s = turtle.Turtle()
     s.shape("turtle")
     s.speed(90)
 
     chao(s)
+    estrela(s)
     ladrilho(s)
     corpo(s)
     teto(s)
@@ -17,7 +19,7 @@ def main():
     arvore(s)
     arvore2(s)
     arvore3(s)
-   
+    
     
     turtle.done()
     
@@ -206,6 +208,7 @@ def tronco(s):
     s.end_fill()
     
 def ladrilho(s):
+    s.color("black")
     s.penup()
     s.goto(25, -100)
     s.pendown()
@@ -216,4 +219,20 @@ def ladrilho(s):
     s.goto(-50, -250)
     s.goto(25, -100)
     s.end_fill()
+    
+def estrela(s):
+    for _  in range(50):  # Você pode ajustar o número de estrelas conforme desejar
+        x = random.randint(-250, 250)  # Posição aleatória no eixo x
+        y = random.randint(0, 250)  # Posição aleatória no eixo y
+        tamanho = random.randint(1, 4)  # Tamanho aleatório da estrela
+        s.penup()
+        s.goto(x, y)
+        s.pendown()
+        s.color("#FFFADE")  # Define a cor da estrela
+        estrela2(s, tamanho)
+        
+def estrela2(s, tamanho):
+    for _  in range(5):
+        s.forward(tamanho)
+        s.right(144)
 main()
